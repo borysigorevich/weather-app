172e3ac482fa2d1968e50d210c4c0f6c27302b93
borysigorevich <borysigorevich@gmail.com>
2024-10-09 16:19:32 +0300

    add geolocation and weatherapi services

---
+++ src/services/constants.ts
@@ -0,0 +1,4 @@
+export const GEOLOCATION_BASE_URL = 'https://api.ipgeolocation.io'
+export const OPENWEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5'
+export const OPENWEATHER_GEO_BASE_URL = 'https://api.openweathermap.org/geo/1.0'
+export const WEATHERAPI_BASE_URL = 'https://api.weatherapi.com/v1'
---
+++ src/services/weather.ts
@@ -0,0 +1,75 @@
+import {apiFetch} from "@/services/api";
+import {OPENWEATHER_BASE_URL} from "@/services/constants";
+
+/**
+ * Fetches weather data based on the location input.
+ *
+ * The `location` parameter should be passed as a correctly formatted string.
+ * Supported formats include:
+ *
+ * - City Name: e.g., `Paris`
+ * - US Zip Code: e.g., `10001`
+ * - UK Postcode: e.g., `SW1`
+ * - Canada Postal Code: e.g., `G2J`
+ * - Latitude and Longitude (decimal degree): e.g., `48.8567,2.3508`
+ * - METAR Code: e.g., `metar:EGLL`
+ * - IATA (Airport Code): e.g., `iata:DXB`
+ * - Auto IP Lookup: `auto:ip`
+ * - IP Address (IPv4 and IPv6 supported): e.g., `100.0.0.1`
+ * - ID returned from Search API: e.g., `id:2801268`
+ *
+ * @param location - The location string in one of the supported formats.
+ * @returns Promise<WeatherDataType>
+ */
+export const getWeather = async (location: string) => {
+    return await apiFetch<WeatherDataType>(`/weather`, {
+        baseUrl: OPENWEATHER_BASE_URL,
+        searchParams: {
+            q: location,
+            key: process.env.WEATHERAPI_KEY as string,
+            days: '5',
+            aqi: 'no',
+            alerts: 'no',
+        },
+    });
+};
+
+export type WeatherDataType = {
+    location: {
+        name: string;
+    };
+    current: {
+        temp_c: number;
+        condition: {
+            text: string;
+            icon: string;
+        };
+        feelslike_c: number;
+    };
+    forecast: {
+        forecastday: ForecastDayType[];
+    };
+}
+
+export type ForecastDayType = {
+    date: string;
+    day: {
+        maxtemp_c: number;
+        mintemp_c: number;
+        daily_chance_of_rain: number;
+        condition: {
+            text: string;
+            icon: string;
+        };
+    };
+    hour: HourlyWeatherType[];
+}
+
+export type HourlyWeatherType = {
+    time: string;
+    temp_c: number;
+    condition: {
+        text: string;
+        icon: string;
+    };
+}
